e1019369d87867440830bb680d8cb025374977ed
AbiRodStr <abistrain23@gmail.com>
2024-11-30 14:08:43 +0100

    Added axios

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#searchFormInput");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
-let searchInput = document.querySelector("#searchFormInput");
-console.log(searchInput.value);
+
 let searchFormElement = document.querySelector("#searchForm");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
